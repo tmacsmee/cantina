7e9f383081ca4a41ca5f3f75b470413f8a947d5a
tmacsmee <troymacsmee@gmail.com>
2026-08-26 15:57:39 +1200

    feat: menu background

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,18 +1,22 @@
-import { Dialog } from "@base-ui/react/dialog";
+import { Dialog } from '@base-ui/react/dialog';
+import { Link } from '@tanstack/react-router';
 import {
   useRef,
   useState,
   type ComponentPropsWithRef,
+  type ElementType,
   type KeyboardEvent,
-} from "react";
-import { useSound } from "use-sound";
-import menuMove from "../assets/audio/menu-move.wav";
+  type ReactElement,
+  type ReactNode,
+} from 'react';
+import { useSound } from 'use-sound';
+import menuMove from '../assets/audio/menu-move.wav';
 
-const menuItems: { text: string; asDialogClose?: boolean }[] = [
-  { text: "Resume", asDialogClose: true },
-  { text: "Options" },
-  { text: "Extras" },
-  { text: "Quit" },
+const menuItems: { text: string; as?: 'close' | 'link'; to?: '/' }[] = [
+  { text: 'Resume', as: 'close' },
+  { text: 'Options' },
+  { text: 'Extras' },
+  { text: 'Quit', as: 'link', to: '/' },
 ];
 
 export default function Menu({
@@ -32,9 +36,9 @@ export default function Menu({
   }
 
   function handleKeyDown(event: KeyboardEvent<HTMLButtonElement>) {
-    if (event.key === "ArrowUp" || event.key === "ArrowDown") {
+    if (event.key === 'ArrowUp' || event.key === 'ArrowDown') {
       event.preventDefault();
-      const increment = event.key === "ArrowUp" ? -1 : 1;
+      const increment = event.key === 'ArrowUp' ? -1 : 1;
 
       const nextIndex =
         (activeIndex + increment + menuItems.length) % menuItems.length;
@@ -52,13 +56,14 @@ export default function Menu({
             the game.
           </Dialog.Description>
           <ul className="font-menu flex flex-col gap-y-2 text-center text-4xl font-bold tracking-wide text-[#1a72c8] [-webkit-text-stroke:5px_rgba(0,0,0,0.75)] [paint-order:stroke_fill]">
-            {menuItems.map(({ text, asDialogClose }, index) => (
+            {menuItems.map(({ text, as }, index) => (
               <MenuItem
                 ref={(item) => {
                   menuItemRefs.current[index] = item;
                 }}
                 key={index}
-                asDialogClose={asDialogClose}
+                as={as}
+                to="/"
                 tabIndex={activeIndex === index ? 0 : -1}
                 onKeyDown={handleKeyDown}
               >
@@ -72,18 +77,24 @@ export default function Menu({
   );
 }
 
-type MenuItemProps = ComponentPropsWithRef<"button"> & {
-  asDialogClose?: boolean;
+type MenuItemProps = ComponentPropsWithRef<'button'> & {
+  as: 'close' | 'link';
+  to: string;
 };
 
-function MenuItem({ asDialogClose, children, ...props }: MenuItemProps) {
+function MenuItem({ as, children, ...props }: MenuItemProps) {
   const [playMenuMove] = useSound(menuMove);
 
   function handleFocus() {
     playMenuMove();
   }
 
-  const Comp = asDialogClose ? Dialog.Close : "button";
+  let Comp: ElementType = 'button';
+  if (as === 'close') {
+    Comp = Dialog.Close;
+  } else if (as === 'link') {
+    Comp = Link;
+  }
 
   return (
     <li>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,8 +4,10 @@ import {
   Link,
   type LinkComponentProps,
 } from '@tanstack/react-router';
+import { type KeyboardEvent, useEffect, useRef, useState } from 'react';
 import useSound from 'use-sound';
 import menuMove from '../assets/audio/menu-move.wav';
+import menuSelect from '../assets/audio/menu-select.wav';
 
 export const Route = createFileRoute('/')({
   component: RouteComponent,
@@ -17,12 +19,48 @@ const menuItems: { text: string; to: string }[] = [
 ];
 
 function RouteComponent() {
+  const menuItemRefs = useRef<(HTMLAnchorElement | null)[]>([]);
+
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  useEffect(() => {
+    menuItemRefs.current[0]?.focus();
+  }, []);
+
+  function moveFocus(index: number) {
+    setActiveIndex(index);
+    menuItemRefs.current[index]?.focus();
+  }
+
+  function handleKeyDown(event: KeyboardEvent<HTMLAnchorElement>) {
+    if (event.key === 'ArrowUp' || event.key === 'ArrowDown') {
+      event.preventDefault();
+      const increment = event.key === 'ArrowUp' ? -1 : 1;
+
+      const nextIndex =
+        (activeIndex + increment + menuItems.length) % menuItems.length;
+      moveFocus(nextIndex);
+    }
+  }
+
   return (
-    <main className="h-screen bg-black">
+    <main className="isolate flex h-screen items-center justify-center bg-black">
+      <div className="animate-starfield fixed inset-0 -z-10 bg-[url('/starfield.png')] bg-size-(--starfield-width) bg-repeat [--starfield-width:250px]" />
+      <div className="animate-starfield fixed inset-0 -z-10 bg-[url('/starfield.png')] bg-size-(--starfield-width) bg-repeat [--starfield-width:300px]" />
+      <div className="animate-starfield fixed inset-0 -z-10 bg-[url('/starfield.png')] bg-size-(--starfield-width) bg-repeat [--starfield-width:350px]" />
       <NavigationMenu.Root>
         <NavigationMenu.List className="font-menu flex flex-col gap-y-2 text-center text-4xl font-bold tracking-wide text-[#1a72c8] [-webkit-text-stroke:5px_rgba(0,0,0,0.75)] [paint-order:stroke_fill]">
-          {menuItems.map(({ text, to }) => (
-            <MenuItem key={text} to={to}>
+          {menuItems.map(({ text, to }, index) => (
+            <MenuItem
+              ref={(item) => {
+                menuItemRefs.current[index] = item;
+              }}
+              key={text}
+              to={to}
+              tabIndex={activeIndex === index ? 0 : -1}
+              autoFocus={index === 0}
+              onKeyDown={handleKeyDown}
+            >
               {text}
             </MenuItem>
           ))}
@@ -32,11 +70,16 @@ function RouteComponent() {
   );
 }
 
-function MenuItem({ children, ...props }: LinkComponentProps) {
+function MenuItem({ children, autoFocus, ...props }: LinkComponentProps) {
   const [playMenuMove] = useSound(menuMove);
+  const [playMenuSelect] = useSound(menuSelect);
 
   function handleFocus() {
     playMenuMove();
+  }
+
+  function handleClick() {
+    playMenuSelect();
   }
 
   return (
@@ -45,6 +88,7 @@ function MenuItem({ children, ...props }: LinkComponentProps) {
         {...props}
         className="focus:animate-menu focus:outline-none"
         onFocus={handleFocus}
+        onClick={handleClick}
       >
         {children}
       </Link>
