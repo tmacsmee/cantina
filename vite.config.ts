e9619ffba3910936d9f3bf34e485892a03a449d5
tmacsmee <troymacsmee@gmail.com>
2026-09-03 00:23:13 +1200

    chore: rename project

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/lsw/',
+  base: '/cantina/',
   assetsInclude: ['**/*.glb'],
   plugins: [
     tanstackRouter({
