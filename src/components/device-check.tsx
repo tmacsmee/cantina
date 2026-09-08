961e794ac4ffdcd1aeb03e2778dd36bf5068ccfd
tmacsmee <troymacsmee@gmail.com>
2026-09-08 20:02:56 +1200

    feat: touchscreen and small screens not supported message

---
+++ src/components/device-check.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useState, type ReactNode } from 'react';
+import unsupportedIcon from '../assets/images/monitor-off.svg';
+
+export default function DeviceCheck({ children }: { children: ReactNode }) {
+  const [supported, setSupported] = useState(true);
+
+  useEffect(() => {
+    function checkDevice() {
+      const widthOK = window.innerWidth >= 1024;
+      const heightOK = window.innerHeight >= 600;
+      const pointerOK = window.matchMedia('(pointer: fine)').matches;
+      setSupported(widthOK && heightOK && pointerOK);
+    }
+
+    checkDevice();
+
+    window.addEventListener('resize', checkDevice);
+    return () => {
+      window.removeEventListener('resize', checkDevice);
+    };
+  }, []);
+
+  if (!supported) {
+    return (
+      <main className="flex h-screen items-center justify-center px-5">
+        <div className="flex max-w-md flex-col items-center gap-y-2 text-center">
+          <img src={unsupportedIcon} className="size-16 opacity-30" />
+          <h1 className="text-center text-2xl font-medium text-gray-900">
+            Device not supported
+          </h1>
+          <p className="text-gray-700">
+            Sorry, this application is not supported on touchscreen devices or
+            devices with small screens.
+          </p>
+        </div>
+      </main>
+    );
+  }
+
+  return children;
+}
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,15 +1,18 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router';
 import AppLifecycleProvider from '../components/app-lifecycle-provider';
+import DeviceCheck from '../components/device-check';
 import SoundProvider from '../components/sound-provider';
+
+export const Route = createRootRoute({ component: RootLayout });
 
 function RootLayout() {
   return (
-    <SoundProvider>
-      <AppLifecycleProvider>
-        <Outlet />
-      </AppLifecycleProvider>
-    </SoundProvider>
+    <DeviceCheck>
+      <SoundProvider>
+        <AppLifecycleProvider>
+          <Outlet />
+        </AppLifecycleProvider>
+      </SoundProvider>
+    </DeviceCheck>
   );
 }
-
-export const Route = createRootRoute({ component: RootLayout });
